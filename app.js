e374d03afe7b0936be2d9626fb8361184d6c6c47
michael-de-wit <michael.b.dewit@gmail.com>
2025-10-21 13:48:41 -0700

    remove extra code

--- app.js
+++ app.js
@@ -14,17 +14,17 @@ const connect = async () => {
 
     while (actionToRun != `5`) {
         console.log(`1. Create a customer\n2. View all customers\n3. Update a customer\n4. Delete a customer\n5. Quit\n`);
-        
+
         actionToRun = prompt(`Number of action to run: `)
-        console.log(`\nYou chose ${actionToRun}\n`);
-        
+        console.log(``);
+
         // const deletedCustomer = await Customer.findByIdAndDelete(`68f7d2e2df0c8a309a6da043`) // delete a given ID
         const customerArray = await Customer.find() // get the customer array
         const customerArrayLength = customerArray.length // find the customer array length, to make sure there is at least 1 customer for the action, if appropriate
         // console.log(`customerArrayLength `, customerArrayLength);
 
         if (actionToRun === `1`) {  // Create a customer
-            
+
             let nameToCreate = prompt(`Name: `)
             let ageToCreate = prompt('Age: ')
 
@@ -32,80 +32,86 @@ const connect = async () => {
                 name: nameToCreate,
                 age: ageToCreate,
             })
-            console.log(`Customer created:\n${createdCustomer}\n`)
-            
+
+            console.log(`\nCustomer created -- ID: ${createdCustomer.id} -- Name: ${createdCustomer.name}, Age: ${createdCustomer.age}\n`)
+
         } else if (actionToRun === `2`) { // View all customers
             customerArray.forEach((cust, index) => {
                 console.log(`${index + 1}. ID: ${cust.id} -- Name: ${cust.name}, Age: ${cust.age}`)
             })
             console.log(`\n`);
-            
+
+            function delay(time) {
+                return new Promise(resolve => setTimeout(resolve, time));
+            }
+
         } else if (actionToRun === `3`) { // Udpdate a customer
             if (customerArrayLength > 0) {
-                
+
                 customerArray.forEach((cust, index) => {
                     console.log(`${index + 1}. ID: ${cust.id} -- Name: ${cust.name}, Age: ${cust.age}`)
                 })
                 console.log(`\n`);
 
                 let customerNumberToUpdate = prompt(`Which customer would you like to update? (Enter the line number) `)
-                
+
                 customerArray.forEach((cust, index) => {
-                    if(index + 1 === Number(customerNumberToUpdate)) {
-                        console.log(`Customer to update: ${customerNumberToUpdate}. ID: ${cust.id} -- Name: ${cust.name}, Age: ${cust.age}`)
+                    if (index + 1 === Number(customerNumberToUpdate)) {
+                        console.log(`\nCustomer to update -- ${customerNumberToUpdate}. ID: ${cust.id} -- Name: ${cust.name}, Age: ${cust.age}`)
                         customerIdToUpdate = cust.id
                     }
                 })
 
-                console.log(`Update the record:\n`);
+                console.log(`Update the record:`);
                 let CustomerNameToUpdate = prompt(`Name: `)
-                let CustomerAgeToUpdate = prompt(`Age: `)                
-                
+                let CustomerAgeToUpdate = prompt(`Age: `)
+
                 const updatedCustomer = await Customer.findByIdAndUpdate(
-                customerIdToUpdate,
-                {
-                    name: CustomerNameToUpdate,
-                    age: CustomerAgeToUpdate,
-                },
-                { new:true }
-            )
+                    customerIdToUpdate,
+                    {
+                        name: CustomerNameToUpdate,
+                        age: CustomerAgeToUpdate,
+                    },
+                    { new: true }
+                )
 
             } else {
                 console.log(`No available customers to update`);
             }
         } else if (actionToRun === '4') { // Delete a customer
             if (customerArrayLength > 0) {
-                
+
                 customerArray.forEach((cust, index) => {
                     console.log(`${index + 1}. ID: ${cust.id} -- Name: ${cust.name}, Age: ${cust.age}`)
                 })
-                console.log(`\n`);
+                console.log(``);
 
                 let customerNumberToDelete = prompt(`Which customer would you like to delete? (Enter the line number) `)
-                console.log(`\n`);
+                console.log(``);
                 customerArray.forEach((cust, index) => {
-                    if(index + 1 === Number(customerNumberToDelete)) {
-                        console.log(`Customer to delete: ${customerNumberToDelete}. ID: ${cust.id} -- Name: ${cust.name}, Age: ${cust.age}\n`)
+                    if (index + 1 === Number(customerNumberToDelete)) {
+                        console.log(`Customer to delete -- ${customerNumberToDelete}. ID: ${cust.id} -- Name: ${cust.name}, Age: ${cust.age}\n`)
                         customerIdToDelete = cust.id
                         customerNameToDelete = cust.name
                         customerAgeToDelete = cust.age
                     }
                 })
-                
+
                 const D = await Customer.findByIdAndDelete(customerIdToDelete) // delete a given ID
-                console.log(`${customerNumberToDelete}. ID: ${customerIdToDelete} -- Name: ${customerNameToDelete}, Age: ${customerAgeToDelete} has been deleted`)
-                
+                console.log(`${customerNumberToDelete}. ID: ${customerIdToDelete} -- Name: ${customerNameToDelete}, Age: ${customerAgeToDelete} has been deleted\n`)
+
             } else {
                 console.log(`No available customers to delete`);
             }
         } else if (actionToRun === '5') { // Quit
             await mongoose.disconnect()
+
             console.log(`disconnected`);
 
         } else {
-            console.log(`Enter a number between 1-5`);
+            console.log(`Enter a number between 1-5\n`);
         }
-        
+
     }
 }
 
